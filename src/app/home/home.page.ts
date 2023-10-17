bf9ed988e38a207d5a33e3b242ce961a686a82ba
tomaXx28 <oleax28@gmail.com>
2023-10-17 17:15:17 -0300

    arreglo del login y home
    navigationExtras.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  state:any;
+  user:any;
+  
+
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    this.activeroute.queryParams.subscribe(params => {
+      this.state=this.router.getCurrentNavigation()?.extras.state;
+        this.user = this.state.user
+        console.log(this.user);
+      })
 }
+}
+
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,50 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { NavigationExtras,Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage{
-  username!: string;
-  password!: String;
-
-  constructor(public alertController: AlertController) { }
-
-  async login() {
-    if (this.username && this.password) {
-      // Mostrar un mensaje emergente con el nombre de usuario
-      const alert = await this.alertController.create({
-        header: 'Bienvenido',
-        message: `Hola, ${this.username}! Has iniciado sesión.`,
-        buttons: ['OK'],
-      });
-
-      await alert.present();
-    } else {
-      // Mostrar un mensaje de error si no se ingresaron credenciales
-      const alert = await this.alertController.create({
-        header: 'Error',
-        message: 'Por favor, ingresa tu nombre de usuario y contraseña.',
-        buttons: ['OK'],
-      });
-
-      await alert.present();
-    }
-
-
-    if (this.isNumeric(this.password.toString())) {
-      // Aquí puedes agregar la lógica de autenticación
-      console.log('Inicio de sesión exitoso');
-    } else {
-      console.log('La contraseña debe ser numérica');
-    }
-  }
-
-  isNumeric(value: any): boolean {
-    return !isNaN(value - parseFloat(value));
-  }
+  user = {
+    usuario:"",
+    password:""
+      }
   
   
-
-}
+    constructor(private router: Router){ }
+  
+    IralHome(){
+    let navigationExtras: NavigationExtras = {
+      state: {user: this.user}
+        };
+    this.router.navigate(['/home'],navigationExtras);
+    }
+  
+  }
