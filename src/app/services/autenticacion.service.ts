43822bd0c1bf4450262b26ffbacdd1e7d520b25f
Valuchio <107000280+Valuchio@users.noreply.github.com>
2023-10-20 14:44:33 -0300

    Update autenticacion.service.ts

--- src/app/services/autenticacion.service.ts
+++ src/app/services/autenticacion.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage-angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 
 @Injectable({
   providedIn: 'root',
